e4dedf5955cefdc96384195a987be2e5a29b5bdd
Li Yang <76434265+hewliyang@users.noreply.github.com>
2023-05-29 15:44:03 +0000

    improve styling & clean up JSX

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,69 +1,16 @@
-import Link from "next/link"
+"use client";
 
-import { siteConfig } from "@/config/site"
-import { Button, buttonVariants } from "@/components/ui/button"
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Terminal } from "lucide-react"
-import { ToastWithAction } from "@/components/ToastWithAction"
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
-import LoginWithIceCream from "@/components/LoginWithIceCream"
 import Sidebar from "@/components/sidebar"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import Image from "next/image";
 
-import React, { Fragment } from 'react';
-
-interface FilePreviewListProps {
-  files: File[];
-}
-
-// const FilePreviewList: React.FC<FilePreviewListProps> = ({ files }) => {
-//   return (
-//     <div className="mt-3 grid grid-cols-4 gap-4">
-//       {files.map((file) => (
-//         <div key={file.id} className="bg-white p-4 shadow rounded">
-//           <img src={file.previewImage} alt={file.name} className="w-full h-32 object-cover mb-2" />
-//           <div className="font-semibold text-gray-800">{file.name}</div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-const FilePreviewList: React.FC<FilePreviewListProps> = ({ files }) => {
-  return (
-    <div className="mt-3 grid grid-cols-4 gap-4">
-      {files.map((file) => (
-        <Link key={file.id} href={`/dashboard`}>
-            <div className="bg-white p-4 shadow rounded hover:bg-gray-100 transition duration-200">
-              <img src={file.previewImage} alt={file.name} className="w-full h-32 object-cover mb-2" />
-              <div className="font-semibold text-gray-800">{file.name}</div>
-            </div>
-        </Link>
-      ))}
-    </div>
-  );
-};
-
-
-// const FilePreviewList: React.FC<FilePreviewListProps> = ({ files }) => {
-//   return (
-//     <div className="mt-3 grid grid-cols-4 gap-4">
-//       {files.map((file) => (
-//         <Link key={file.id} href={`/dashboard`}>
-//           <a className="cursor-pointer">
-//             <div className="bg-white p-4 shadow rounded">
-//               <img src={file.previewImage} alt={file.name} className="w-full h-32 object-cover mb-2" />
-//               <div className="font-semibold text-gray-800">{file.name}</div>
-//             </div>
-//           </a>
-//         </Link>
-//       ))}
-//     </div>
-//   );
-// };
-
-export const Text = () => <Fragment>Most Recent Files</Fragment>;
-
-const sampleFiles: File[] = [
+const sampleFiles = [
     {
       id: '1',
       name: 'Document 1',
@@ -80,17 +27,41 @@ const sampleFiles: File[] = [
       previewImage: 'https://picsum.photos/id/237/200/300',
     },
     // Add more sample files as needed, todo: link them to individual document pages, preview pdf small
-  ];
+];
 
-export default function IndexPage() {
+type cardProps = {
+  id: string
+  name: string
+  previewImage: string
+}
+
+function DisplayCard( {id, name, previewImage}: cardProps) {
   return (
-    <section className="flex">
-    
-    <Sidebar />
-    <section className="container mt-6">
-      <Text />
-      <FilePreviewList files = {sampleFiles} />
-    </section>
-    </section>
+    <Card key={id}>
+      <CardHeader>
+        <CardTitle>{name}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Image src={previewImage} alt="Card Image" width={200} height={200} className="rounded-lg" />
+      </CardContent>
+    </Card>
+  )
+}
+
+export default function Dashboard() {
+  return (
+    <div className="flex">
+      <Sidebar />
+
+      <div className="container grid grid-cols-6 gap-6 p-4">
+        {sampleFiles.map((item) => (
+          <div key={item.id}>
+            <DisplayCard {...item} />
+          </div>
+        ))}
+        <Button className="absolute top-15 right-10">Upload Image</Button>
+      </div>
+
+    </div>
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,23 +1,9 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { Terminal } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
 import { CreateAccount } from "@/components/create-account"
 
 export default function IndexPage() {
   return (
     <section className="container flex justify-between gap-6 pb-8 px-12 pt-6 md:py-12">
-
-      {/* <Alert className="max-w-lg" variant="destructive">
-        <Terminal className="h-4 w-4" />
-        <AlertTitle>Heads up!</AlertTitle>
-        <AlertDescription>
-          Prepping for hackton!
-        </AlertDescription>
-      </Alert> */}
       
       <div className="mt-20 flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
@@ -29,47 +15,13 @@ export default function IndexPage() {
         <Separator />
 
         <p className="text-muted-foreground max-w-[700px] text-lg sm:text-xl">
-           Secure, fast, free (for now) <br className="hidden sm:inline" />
-           Helping traditional businesses go digital, one step at a time.
+          Secure, fast, free (for now) <br className="hidden sm:inline" />
+          Helping traditional businesses go digital, one step at a time.
         </p>
       </div>
-      <div className="mt-2 flex gap-4">
-        {/* <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants({ size: "lg" })}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline", size: "lg" })}
-        >
-          GitHub
-        </Link> */}
 
-        {/* <Link
-          href={"/signup"}
-          // target="_blank"
-          rel="noreferrer"
-          className={buttonVariants({ size: "lg" })}
-        >
-          Sign Up
-        </Link>
-        <Link
-          href={"/login"}
-          // target="_blank"
-          rel="noreferrer"
-          className={buttonVariants({ variant: "outline", size: "lg" })}
-        >
-          Login
-        </Link> */}
-      </div>
       <div className="flex mr-8">
-      <CreateAccount/>
+        <CreateAccount/>
       </div>
       
     </section>
